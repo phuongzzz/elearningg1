111d7620b05b5d34d900451e68662c2744432234
phuongzzz <yesterdayoncemore23@gmail.com>
2017-11-20 01:47:25 +0700

    js fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require turbolinks
 //= require_tree .
 
- $(document).ready(function () {
+ $(document).on('turbolinks:load', function() {
    $('#sidebarCollapse').on('click', function () {
      $('#sidebar').toggleClass('active');
      $(this).toggleClass('active');
